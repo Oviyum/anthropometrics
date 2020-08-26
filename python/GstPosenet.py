b1c2fb08f3f99d812217962b1f8236a11a72e060
Eshaan Bhansali <eshaanb@live.com>
2020-08-26 17:24:18 +0530

    added comments

--- python/GstPosenet.py
+++ python/GstPosenet.py
@@ -32,7 +32,7 @@ class GstPosenet(GstBase.BaseTransform):
     
     
     __gstmetadata__ = ('GstPosenet Python','Transform',
-                      'example gst-python element that can modify the buffer gst-launch-1.0 videotestsrc ! TestTransform2 ! videoconvert ! xvimagesink', 'dkl')
+                      'gst-python element that can calculate the height of a person', 'dkl')
 
     __gsttemplates__ = (Gst.PadTemplate.new("src",
                                            Gst.PadDirection.SRC,
@@ -49,13 +49,13 @@ class GstPosenet(GstBase.BaseTransform):
                   None,
                   GObject.ParamFlags.READWRITE)
     }
-    
     def __init__(self):
         self.Q = None
         self.width = -1
         self.height = -1
         self.calibration = None
-        self.engine = PoseEngine('python/project/models/mobilenet/posenet_mobilenet_v1_075_721_1281_quant_decoder_edgetpu.tflite')
+        #path to the tflite posenet model
+        self.engine = PoseEngine('python/project-posenet/models/mobilenet/posenet_mobilenet_v1_075_721_1281_quant_decoder_edgetpu.tflite')
         self.max_pose_detections = 20
         self.lcameramtx = None
         self.ldist = None
@@ -65,14 +65,17 @@ class GstPosenet(GstBase.BaseTransform):
         self.rdist = None
         self.rrectification = None
         self.rprojection = None
-        self.f = open('data.txt', 'w')
-        self.data = []
         self.avg = 0
         self.alpha = 0.1
         self.var = 0
+        #height and width of the posenet model
+        self.modelw = 1281
+        self.modelh = 721
     def calculate_height(self, lx1, ly1, lx2, ly2, rx1, ry1, rx2, ry2):
 
         points2d = np.array([[lx1, ly1, lx1-rx1, 1], [lx2, ly2, lx2-rx2, 1]], dtype=np.float32).T
+
+        #use the Q matrix to project the 2d coordinates to 3d
         points3d = self.Q.dot(points2d)
         x1 = points3d[0][0]/points3d[3][0]
         y1 = points3d[1][0]/points3d[3][0]
@@ -82,56 +85,38 @@ class GstPosenet(GstBase.BaseTransform):
         y2 = points3d[1][1]/points3d[3][1]
         z2 = points3d[2][1]/points3d[3][1]
 
-        #print(points2d)
-       # print(points3d)
-
-        #print((x1, y1, z1), (x2, y2, z2))
-
-
+        #use 3d coordinates to calculate the distance
         distance = np.sqrt((x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2)
         return distance
     
-    def pose(self, img, poses, i):
+    def pose(self, img, posearray, i):
         newimg = Image.fromarray(img, 'RGB')
-        #newimg.save('1.jpg')
-        newimg = newimg.resize((1281, 721), Image.NEAREST)
-        #newimg.save('2.jpg')
-        #cv2.imwrite('3.jpg', img)
+        newimg = newimg.resize((self.modelw, self.modelh), Image.NEAREST)
         lefteye = None
         leftankle = None
         poses, inference_time = self.engine.DetectPosesInImage(np.uint8(newimg))
-        print('Inference time: %.fms' % inference_time)
-
         for pose in poses:
             if pose.score < 0.4: continue
-            print('\nPose Score: ', pose.score)
             for label, keypoint in pose.keypoints.items():
-                #print(' %-20s x=%-4d y=%-4d score=%.1f' % 
-                 #       (label, keypoint.yx[1], keypoint.yx[0], keypoint.score))
-                cv2.circle(img, (int(keypoint.yx[1]/(1281/720)), int(keypoint.yx[0]*(1280/721))), 1, 255, 3)
-                #im2 = cv2.imread('2.jpg')
-                #cv2.circle(im2, (int(keypoint.yx[1]), int(keypoint.yx[0])), 1, 255, 3)
-                #cv2.imwrite('5.jpg', im2)
+                cv2.circle(img, (int(keypoint.yx[1]/(self.modelw/(self.width/2))), int(keypoint.yx[0]*(self.height/self.modelh))), 1, 255, 3)
                 if label == 'left eye':
                     lefteye = keypoint.yx
                 if label == 'left ankle':
                     leftankle = keypoint.yx
-                #cv2.imwrite('4.jpg', img)
-        poses[i] = (lefteye, leftankle)
+        posearray[i] = (lefteye, leftankle)
         return (lefteye, leftankle)
 
-
+#exponential moving average and moving variance with a weight alpha. outliers which are more than one standard deviation from the mean are clipped to one standard deviation from the mean.
     def ema(self, h):
-
-        self.var = (1-self.alpha)*(self.var + self.alpha*((h - self.avg)**2))
-        lim = 2 * np.sqrt(self.var)
-        if h > self.avg:
-            h = min(h, self.avg + lim)
-        else:
-            h = max(h, self.avg - lim)
         if self.avg == 0:
             self.avg = h
         else:
+            lim = np.sqrt(self.var)
+            if h > self.avg and not self.var == 0:
+                h = min(h, self.avg + lim)
+            elif h < self.avg and not self.var == 0:
+                h = max(h, self.avg - lim)
+            self.var = (1-self.alpha)*(self.var + self.alpha*((h - self.avg)**2))
             self.avg = self.alpha*h + (1- self.alpha)*self.avg
         return self.avg
 
@@ -166,10 +151,12 @@ class GstPosenet(GstBase.BaseTransform):
 
             A = np.ndarray(buf.get_size(), dtype = np.uint8, buffer = info.data)
             A = A.reshape(self.height, self.width, 3).squeeze()
+            #left and right frames are separated from the stereo video
             limg = A[:, :self.width//2 - 1]
             rimg = A[:, self.width//2:]
             img = A
-            poses = []
+            poses = [None, None]
+            #thread to allow left and right pose detection to take place simultaneously.
             t = threading.Thread(target=self.pose, args=(rimg, poses, 1))
             t.start()
             self.pose(limg, poses, 0)
@@ -177,13 +164,16 @@ class GstPosenet(GstBase.BaseTransform):
 
             lpose = poses[0]
             rpose = poses[1]
-            
+            #ratios of width and height to model width and height to scale the detected coordinates
+            xr = (self.width/2)/self.modelw
+            yr = self.height/self.modelh
             if lpose[0] is not None and rpose[0] is not None:
                 ([ly1, lx1], [ly2, lx2]) = lpose
                 ([ry1, rx1], [ry2, rx2]) = rpose
-                lpoints = np.array([[lx1/(1281/720), ly1*(1280/721)], [lx2/(1281/720), ly2*(1280/721)]], dtype=np.float32)
-                rpoints = np.array([[rx1/(1281/720), ry1*(1280/721)], [rx2/(1281/720), ry2*(1280/721)]], dtype=np.float32)
+                lpoints = np.array([[lx1*xr, ly1*yr], [lx2*xr, ly2*yr]], dtype=np.float32)
+                rpoints = np.array([[rx1*xr, ry1*yr], [rx2*xr, ry2*yr]], dtype=np.float32)
 
+                #the Q matrix is in the same coordinate system as the undistorted image, not the actual image, so we need to first project the detected coordinates into this undistorted system.
                 lpoints = cv2.undistortPoints(lpoints, self.lcameramtx, self.ldist, None, self.lrectification, self.lprojection)
                 rpoints = cv2.undistortPoints(rpoints, self.rcameramtx, self.rdist, None, self.rrectification, self.rprojection)
                 lx1 = lpoints[0][0][0]
@@ -195,16 +185,8 @@ class GstPosenet(GstBase.BaseTransform):
                 rx2 = rpoints[1][0][0]
                 ry2 = rpoints[1][0][1]
                 h = self.calculate_height(lx1, ly1, lx2, ly2, rx1, ry1, rx2, ry2)
-                print('ema')
+                print('height:')
                 print(self.ema(h))
-                self.data.append(h)
-                if len(self.data) > 10:
-                    del self.data[0]
-                print('ma')
-                print(np.mean(self.data))
-                print('h')
-                print(h)
-                self.f.write(str(h) + "\n")
             else:
                 print('Pose not detected')
             buf.unmap(info)
